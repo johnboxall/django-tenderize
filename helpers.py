ba89e4557b55ef4a750aa497398f9fc4ef4ac149
John Boxall <johnboxall@Handi-Mobility-2.local>
2008-12-05 08:55:25 -0800

    Switched helpers.tenderize_response to use django.utils.http.cookie_date

--- helpers.py
+++ helpers.py
@@ -1,9 +1,9 @@
 import hmac
 import sha
-from time import mktime
-from datetime import datetime, timedelta
+from time import time
 
 from django.conf import settings
+from django.utils.http import cookie_date
 
 # help.yourapp.com/user@gmail.com/1228117891
 FORMAT = "%s/%s/%s"
@@ -18,9 +18,9 @@ def tender_hash(email, expires, tender=settings.TENDER_DOMAIN, secret=settings.T
     
 def tenderize_response(response, email):
     """Adds tender cookies to the response"""
-    expires = datetime.now() + timedelta(seconds=settings.TENDER_COOKIE_AGE)
-    tender_expires = int(mktime(expires.timetuple())) # Tender wants expires in epoch
-    cookie_expires = expires.strftime("%a, %d-%b-%Y %H:%M:%S UTC") # Set cookie wants expires in UTC
+    expires = time() + settings.TENDER_COOKIE_AGE
+    tender_expires = int(expires)  # Tender wants expires in epoch seconds
+    cookie_expires = cookie_date(expires)  # Cookie time for the cookie
     hashed = tender_hash(email, tender_expires)
     response.set_cookie('tender_email', email, expires=cookie_expires, domain=settings.TENDER_COOKIE_DOMAIN)
     response.set_cookie('tender_expires', tender_expires, expires=cookie_expires, domain=settings.TENDER_COOKIE_DOMAIN)
