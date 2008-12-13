2a295fcc25800bfb645d07bc3bc91004ff7e9da6
John Boxall <johnboxall@Handi-Mobility-2.local>
2008-12-12 17:33:15 -0800

    views.py: refactored out login code by checking the response type.

--- views.py
+++ views.py
@@ -1,42 +1,20 @@
-from django.conf import settings
+from django.http import HttpResponseRedirect
+from django.contrib.auth.views import login
 from django.contrib.auth import REDIRECT_FIELD_NAME
 from django.contrib.auth.forms import AuthenticationForm
-from django.shortcuts import render_to_response
-from django.contrib.sites.models import Site, RequestSite
-from django.http import HttpResponseRedirect
-from django.template import RequestContext
 from django.views.decorators.cache import never_cache
 
-from tenderize.helpers import tenderize_response
+from jungle.tender.helpers import tenderize_response
 
 def login_and_tenderize(request, template_name='registration/login.html', redirect_field_name=REDIRECT_FIELD_NAME):
-    "Displays the login form and handles the login action. Also logs you into Tender."
-    redirect_to = request.REQUEST.get(redirect_field_name, '')
-    if request.method == "POST":
+    "Displays the login form and handles the login action. Sets Tender cookies if successful."
+    response = login(request, template_name, redirect_field_name)    
+    # login returns a HttpResponseRedirect if successful.
+    if isinstance(response, HttpResponseRedirect):
+        # Get email from the logged in user.
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
-            # Light security check -- make sure redirect_to isn't garbage.
-            if not redirect_to or '//' in redirect_to or ' ' in redirect_to:
-                redirect_to = settings.LOGIN_REDIRECT_URL
-            from django.contrib.auth import login
             user = form.get_user()
-            login(request, user)
-            if request.session.test_cookie_worked():
-                request.session.delete_test_cookie() 
-            # Tender in here
-            response = HttpResponseRedirect(redirect_to)
             response = tenderize_response(response, user.email)
-            return response
-    else:
-        form = AuthenticationForm(request)
-    request.session.set_test_cookie()
-    if Site._meta.installed:
-        current_site = Site.objects.get_current()
-    else:
-        current_site = RequestSite(request)
-    return render_to_response(template_name, {
-        'form': form,
-        redirect_field_name: redirect_to,
-        'site_name': current_site.name,
-    }, context_instance=RequestContext(request))
+    return response
 login_and_tenderize = never_cache(login_and_tenderize)
