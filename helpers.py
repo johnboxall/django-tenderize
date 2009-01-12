74d2d70ca1e9fcf6bc608bd41632e9d4aa6b9bc5
John Boxall <john@handimobility.ca>
2009-01-11 23:47:20 -0800

    Added chrisdrackett's `tender_xxx` support.

--- helpers.py
+++ helpers.py
@@ -6,26 +6,35 @@ from django.conf import settings
 from django.utils.http import cookie_date
 
 # help.yourapp.com/user@gmail.com/1228117891
-FORMAT = "%s/%s/%s"
+HASH_FORMAT = "%s/%s/%s"
+COOKIE_FORMAT = "tender_%s"
+DOMAIN = settings.TENDER_COOKIE_DOMAIN
+SECRET = settings.TENDER_SECRET
+AGE = settings.TENDER_COOKIE_AGE
 
-def tender_hash(email, expires, tender=settings.TENDER_DOMAIN, secret=settings.TENDER_SECRET):
+def tender_hash(email, expires, tender=DOMAIN, secret=SECRET):
     """Calculates the tender hash."""
-    s = FORMAT % (tender, email, expires)
+    s = HASH_FORMAT % (tender, email, expires)
     sig = hmac.new(secret, digestmod=sha)
     sig.update(s)
     tender_hash = sig.hexdigest()
     return tender_hash
     
-def tenderize_response(response, email):
-    """Adds tender cookies to the response"""
-    expires = time() + settings.TENDER_COOKIE_AGE
-    tender_expires = int(expires)  # Tender wants expires in epoch seconds
-    cookie_expires = cookie_date(expires)  # Cookie time for the cookie
+def tenderize_response(response, email, extra_cookies=None):
+    """Adds tender cookies to `response.`"""
+    expires = time() + AGE
+    # Tender wants expires in epoch seconds, expires is set with `cookie_date`
+    tender_expires = int(expires)
+    cookie_expires = cookie_date(expires)
     hashed = tender_hash(email, tender_expires)
-    response.set_cookie('tender_expires', tender_expires, expires=cookie_expires, domain=settings.TENDER_COOKIE_DOMAIN)
-    response.set_cookie('tender_hash', hashed, expires=cookie_expires, domain=settings.TENDER_COOKIE_DOMAIN)
-    response.set_cookie('tender_email', email, expires=cookie_expires, domain=settings.TENDER_COOKIE_DOMAIN)
-    # response.set_cookie() incorrectly sets coded_value to "<email>"
-    # we need to override that default value be just <email>
+    cookies = dict(expires=expires, hash=hashed, email=email)
+    # Add extra Tender cookies: http://tinyurl.com/8vwxyw
+    if extra_cookies is not None:
+        cookies.update(extra_cookies)
+    for key, value in cookies.iteritems():
+        cookie = COOKIE_FORMAT % key
+        response.set_cookie(cookie, value, expires=cookie_expires, domain=DOMAIN)
+    # `response.set_cookie()` incorrectly adds questions to the email cookie.
+    # To remove the quotes we set the value again.
     response.cookies['tender_email'].coded_value = email    
     return response
--- tests.py
+++ tests.py
@@ -1,11 +1,27 @@
 import unittest
 
-from tenderize.helpers import tender_hash
+from django.http import HttpResponse
 
-class TenderHashTestCase(unittest.TestCase):
+from tenderize.helpers import tender_hash, tenderize_response
+from tenderize.views import login_and_tenderize
+
+class TenderizeTestCase(unittest.TestCase):
+    def setUp(self):
+        self.user = 'user'
+        self.email = 'user@gmail.com'
+        self.password = 'password'
+        self.tender = 'help.yourapp.com'
+        self.expires = 1228117891
+        self.secret = 'monkey'
+
     def testTenderHash(self):
-        email = 'user@gmail.com'
-        tender = 'help.yourapp.com'
-        expires = 1228117891
-        secret = 'monkey'
-        self.assertEquals(tender_hash(email, expires, tender, secret), '1937bf7e8dc9f475cc9490933eb36e5f7807398a')
+        result = tender_hash(self.email, self.expires, self.tender, self.secret)
+        self.assertEquals(result, '1937bf7e8dc9f475cc9490933eb36e5f7807398a')
+
+    def testTenderizeResponse(self):
+        response = HttpResponse('Test Response')
+        response = tenderize_response(response, self.email, {'user': self.user})
+        self.assertEqual(response.cookies['tender_email'].value, self.email)
+        self.assertEqual(response.cookies['tender_user'].value, self.user)
+        self.assertTrue('tender_expires' in response.cookies)
+        self.assertTrue('tender_hash' in response.cookies)
